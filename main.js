c223959916d1b54fdbfe46c2df39cb9f31348adc
khoitruong9802 <khoitruong9802@gmail.com>
2024-12-02 02:01:47 +0700

    Add secret feature

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
 
 app.get("/data", (req, res) => {
   res.status(200).json({
     message: "ok ban oi",
+  });
+});
+
+app.get("/secret", (req, res) => {
+  res.status(200).json({
+    message: process.env.SECRET,
   });
 });
 
